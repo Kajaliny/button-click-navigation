b8fb0b0680db93c7be16e04a8870754673230b80
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-11-26 09:32:35 +0000

    error correct

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,6 @@
 import Login from './components/Login';
 import Dashboard from '.components/Dashboard';
-
+import{BrowserRouter as Router, Routes, Route, Link, NavLink} from 'react-router-dom';
 function App(){
     return(
         <div className="App">
