e9baf415f961ea89eaf66755bf2f24a89bca6d06
Kajaliny <139700105+Kajaliny@users.noreply.github.com>
2024-11-26 09:13:36 +0000

    login button click update

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,12 @@ export default function Login(){
     const navigate = useNavigate();
     const authenticate=()=>{
         navigate('/dash')
-        
+
     }
+    return(
+        <div>
+            <h1> Login here</h1>
+            <button onClick={authenticate}> Login</button>
+        </div>
+    );
 }
